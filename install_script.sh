13c3cc5af64974f1a247d1732f772ce22cc0b29a
Jove Systems <32963077+JoveSystems@users.noreply.github.com>
2020-08-28 13:29:08 +0100

    Update language

--- install_script.sh
+++ install_script.sh
@@ -5,7 +5,7 @@ START_SCRIPT_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)/simple_kernel.py
 PYTHON_PATH=$(which python)
 CONTENT='{
    "argv": ["'${PYTHON_PATH}'", "'${START_SCRIPT_PATH}'", "{connection_file}"],
-                "display_name": "simple_kernel",
-                "language": "simple_kernel"
+                "display_name": "Kore",
+                "language": "Kore"
 }'
 echo $CONTENT > ~/.ipython/kernels/simple_kernel/kernel.json
